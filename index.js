473f423f01593cf3d23197a93e45c61bbb5276b5
Uray Jordi L <u.jordi@smooets.com>
2020-04-05 09:52:31 +0700

    create a function to listen to dm and forward it to the specified channel

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Ready');
+  client.user.setPresence({
+    activity: { name: 'Listening to DM'},
+    status: 'online'
+  }).catch(console.error);
+  
+  console.log('Bot Online');
 });
 
 client.login(Config.token);
 
 client.on('message', message => {
-  console.log("Channel: " + message.channel.name);
-  console.log("User: " + message.author.username);
-  console.log("Message: " + message.content);
+  console.log('Author: ', message.author.username);
+  console.log('Channel: ', message.channel.name || 'dm');
+  console.log('Message: ', message.content);
+
+  client.channels.cache.map(channel => {
+    if (
+        (channel.type === 'text' && channel.name === 'bot-message-watcher-inbox')
+        && (message.author.id !== client.user.id)
+    ) {
+      channel.send(
+        `Sender: \`${message.author.username}\`\rMessage:\r\`\`\`${message.content}\`\`\``
+        ).catch(console.error);
+    }
+  });
 });
